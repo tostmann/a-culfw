f1bf684ba42c073e7e52de786f61bb55b71fe63c
Dirk Tostmann <tostmann@busware.de>
2026-02-06 00:38:24 +0100

    Entferne alten ESP32 Versuch

--- culfw/ESP32/avr/boot.h
+++
@@ -1,7 +0,0 @@
-#ifndef _AVR_BOOT_H_
-#define _AVR_BOOT_H_
-#include <Arduino.h>
-#define BOOTLOADER_SECTION
-static uint8_t boot_special_request = 0;
-static uint8_t boot_key = 0;
-#endif
--- culfw/ESP32/avr/power.h
+++
@@ -1,6 +0,0 @@
-#ifndef _AVR_POWER_H_
-#define _AVR_POWER_H_
-#include <stdint.h>
-typedef enum { clock_div_1 = 0 } clock_div_t;
-static inline void clock_prescale_set(clock_div_t x) { (void)x; }
-#endif
