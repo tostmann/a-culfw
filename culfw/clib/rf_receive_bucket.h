f610396c926e12cc650ec31d143d8cac852a75b2
AI Builder Agent <ai-builder@tostmann.de>
2026-02-10 15:50:37 +0100

    Auto-Backup: Edit culfw/clib/rf_receive_bucket.h

--- culfw/clib/rf_receive_bucket.h
+++ culfw/clib/rf_receive_bucket.h
@@ -47,7 +47,7 @@
 #define TSCALE(x)  (x/16)      // Scaling time to enable 8bit arithmetic
 
 
-#define TDIFF      TSCALE(200) // tolerated diff to previous/avg high/low/total
+#define TDIFF      TSCALE(400) // tolerated diff to previous/avg high/low/total
 
 #define STATE_RESET   0
 
